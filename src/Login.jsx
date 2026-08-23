0cad6f0172588f42d632e5702a48f09a5679a49b
bontusvibar-stack <bontusvibar@gmail.com>
2026-08-23 16:52:50 +0700

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from './AuthContext';
 
 export default function Login() {
   const { login, loginWithGoogle, currentUser } = useAuth();
